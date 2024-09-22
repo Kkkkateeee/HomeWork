5edb6effc905f5061c5a753dddd17070cf93bedf
Kkkkateeee <kt.spbu@gmail.com>
2024-09-22 20:11:41 +0300

    add tests

--- src/Tests/Tests.fs
+++ src/Tests/Tests.fs
@@ -2,10 +2,34 @@ namespace Tests
 
 open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
+open Library.Say  // подключаем пространство имен
+
 
 [<TestClass>]
 type TestClass () =
 
-    [<TestMethod>]
-    member this.TestMethodPassing () =
-        Assert.IsTrue(true);
+    [<TestMethod>]  // F0
+    member this.Fib_0 () =
+        Assert.AreEqual(0L, fibonacci(0))
+    
+    [<TestMethod>]  // F1
+    member this.Fib_1 () =
+        Assert.AreEqual(1L, fibonacci(1))
+    
+    [<TestMethod>]  // F2
+    member this.Fib_2 () =
+        Assert.AreEqual(1L, fibonacci(2))
+    
+    [<TestMethod>]  // F3
+    member this.Fib_3 () =
+        Assert.AreEqual(2L, fibonacci(3))
+    
+    [<TestMethod>]  // F30
+    member this.Fib_30 () =
+        Assert.AreEqual(832040L, fibonacci(30))
+
+    [<TestMethod>]  // F(-1)
+    member this.Fib_negative () =
+        let ex = Assert.ThrowsException<System.Exception>(fun () -> fibonacci -1 :> obj)
+        Assert.AreEqual("Числа Фибоначчи для отрицательных n не определены\n", ex.Message)
+    
--- src/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.fs
+++ src/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Tests")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48ee3beba4a4432b91c0279882c9acc3889808e1")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Tests")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Tests")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
