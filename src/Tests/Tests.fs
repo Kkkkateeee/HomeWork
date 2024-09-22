aaae0a4426bd23ee63fe2ce815b44a53adaa7d93
Kkkkateeee <kt.spbu@gmail.com>
2024-09-22 19:54:57 +0300

    fixed tests

--- src/Tests/Tests.fs
+++ src/Tests/Tests.fs
@@ -2,10 +2,25 @@ namespace Tests
 
 open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
+open Library.Say  // подключаем пространство имен
+
 
 [<TestClass>]
 type TestClass () =
 
-    [<TestMethod>]
-    member this.TestMethodPassing () =
-        Assert.IsTrue(true);
+    [<TestMethod>]  // 0!
+    member this.TestFactorial_Zero () =
+        Assert.AreEqual(1, factorial(0))
+
+    [<TestMethod>]  // 1!
+    member this.TestFactorial_One () =
+        Assert.AreEqual(1, factorial(1))
+
+    [<TestMethod>]  // 5!
+    member this.TestFactorial_Five () =
+        Assert.AreEqual(120, factorial(5))
+
+    [<TestMethod>]  // (-1)!
+    member this.Factorial_negative () =
+        let ex = Assert.ThrowsException<System.Exception>(fun () -> factorial -1 :> obj)
+        Assert.AreEqual("Факториал отрицательных чисел не вычисляется этой программой\n", ex.Message)
