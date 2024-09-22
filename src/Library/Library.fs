8df80c1244ac6a385be321a218ca596e391bc276
Kkkkateeee <kt.spbu@gmail.com>
2024-09-22 20:14:45 +0300

    add function

--- src/Library/Library.fs
+++ src/Library/Library.fs
@@ -1,5 +1,20 @@
 ﻿namespace Library
 
 module Say =
-    let hello name =
-        printfn "Hello %s" name
+    let rec Quick_sort (arr: int[]) =
+
+        if Array.length arr <= 1  then arr
+
+        else
+            let pivot = Array.last arr  // последний элемент массива
+
+            let smaller = ResizeArray<int>()
+            let bigger = ResizeArray<int>()
+            let equal = ResizeArray<int>()
+
+            for i in 0 .. Array.length arr - 1 do
+                if arr[i] < pivot then smaller.Add(arr[i])
+                elif arr[i] > pivot then bigger.Add(arr[i])
+                else equal.Add(arr[i])
+            
+            Array.append (Array.append (Quick_sort (smaller.ToArray()) ) (equal.ToArray())) (Quick_sort (bigger.ToArray()))
