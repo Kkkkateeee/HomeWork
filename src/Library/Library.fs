345e3fa5df26b1205eb620efa17723cb878bc7f5
Kkkkateeee <kt.spbu@gmail.com>
2024-09-22 20:06:19 +0300

    add function

--- src/Library/Library.fs
+++ src/Library/Library.fs
@@ -1,5 +1,18 @@
 ﻿namespace Library
 
 module Say =
-    let hello name =
-        printfn "Hello %s" name
+    let bubble_sort (array : int[]) =
+    
+        let mutable array_len = array.Length
+        let mutable swapped = true
+    
+        while swapped do
+            swapped <- false 
+            for i in 0 .. array_len - 2 do
+                if array[i] > array[i + 1] then
+                    let elem = array[i]
+                    array[i] <- array[i + 1]
+                    array[i + 1] <- elem
+                    swapped <- true 
+            array_len <- array_len - 1 
+
