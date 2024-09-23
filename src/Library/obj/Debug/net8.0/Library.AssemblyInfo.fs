c2325994c5d63535c0354d8b94b8004d3611e63c
Kkkkateeee <kt.spbu@gmail.com>
2024-09-23 16:28:05 +0300

    .

--- src/Library/obj/Debug/net8.0/Library.AssemblyInfo.fs
+++ src/Library/obj/Debug/net8.0/Library.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Library")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8df80c1244ac6a385be321a218ca596e391bc276")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad3e9698926b0ad56b545a42bb396c34f0edaac2")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Library")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Library")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
