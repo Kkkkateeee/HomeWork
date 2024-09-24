a0c87d02cf8c8796d261519f8582bdfdd4ab5e35
Kate Titova <kt.spbu@gmail.com>
2024-09-24 19:50:14 +0300

    Tests.fs commelts lang RU -> EN

--- src/Tests/Tests.fs
+++ src/Tests/Tests.fs
@@ -2,37 +2,37 @@ namespace Tests
 
 open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
-open Library.bubble_sort  // подключаем пространство имен
+open Library.bubble_sort  // add namespace
 
 
 [<TestClass>]
 type TestClass () =
 
-    [<TestMethod>]  // пустой массив
+    [<TestMethod>]  // empty array
     member this.TestEmpty() =
         let arr = [| |]
         bubble_sort arr
         Assert.IsTrue(Seq.forall2 (=) arr [| |])
 
-    [<TestMethod>]  // отсортированный массив
+    [<TestMethod>]  // sorted array
     member this.TestSortedArray() =
         let arr = [|1; 2; 3; 4; 5|]
         bubble_sort arr
         Assert.IsTrue(Seq.forall2 (=) arr [|1; 2; 3; 4; 5|])
 
-    [<TestMethod>]  // массив обратной сортироваки
+    [<TestMethod>]  // reverse sorted array
     member this.TestReverseSortedArray() =
         let arr = [|5; 4; 3; 2; 1|]
         bubble_sort arr
         Assert.IsTrue(Seq.forall2 (=) arr [|1; 2; 3; 4; 5|])
         
-    [<TestMethod>]  // массив произвольной сортировки
+    [<TestMethod>]  // unsorted array
     member this.TestUnsortedArray() =
         let arr = [|3; 1; 4; 1; 5; 9; 2; 6; 5; 3; 5|]
         bubble_sort arr
         Assert.IsTrue(Seq.forall2 (=) arr [|1; 1; 2; 3; 3; 4; 5; 5; 5; 6; 9|])
 
-    [<TestMethod>]  // массив с повторяющимися элементами
+    [<TestMethod>]  // array with repeating elements
     member this.TestArrayWithDuplicates() =
         let arr = [|4; 2; 2; 8; 3; 3; 1|]
         bubble_sort arr
