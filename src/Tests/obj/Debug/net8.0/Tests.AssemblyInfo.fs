73b0b854f2d7fea916e51d93985b3108fd9ca521
Kkkkateeee <kt.spbu@gmail.com>
2024-09-24 19:58:58 +0300

    add gitignore and remove cached files

--- src/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.fs
+++ src/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Tests")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+718a4f7a09319cfe575ff4c52119c200bb149d25")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Tests")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Tests")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
