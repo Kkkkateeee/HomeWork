7d7be72a8b77c67d82849c762c0ad19e42a92c75
Kkkkateeee <kt.spbu@gmail.com>
2024-09-30 20:39:55 +0300

    add functions and tests

--- src/Library/Library.fs
+++ src/Library/Library.fs
@@ -1,5 +1,130 @@
 ﻿namespace Library
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+
+module factorial =
+    let rec factorial n =
+        if n < 0 then
+            failwith "The factorial of negative numbers is not calculated by this program\n"
+        elif n = 0 || n = 1 then
+            1
+        else
+            n * factorial (n - 1)
+
+
+module sorts =
+
+    let swap (arr: 't[]) i j =
+        let elem = arr[i]
+        arr[i] <- arr[j]
+        arr[j] <- elem
+
+
+    let bubble_sort (arr: 't[]) =
+
+        let mutable array_len = arr.Length
+        let mutable swapped = true
+
+        while swapped do
+            swapped <- false
+
+            for i in 0 .. array_len - 2 do
+                if compare arr[i] arr[i + 1] > 0 then
+                    swap arr i (i + 1)
+                    swapped <- true
+
+            array_len <- array_len - 1
+
+
+    let rec quick_sort (arr: 't[]) left right =
+
+        if left < right then
+            let pivot = arr[(left + right) / 2]
+            let mutable i = left
+            let mutable j = right
+
+            while i <= j do
+                while compare arr[i] pivot < 0 do
+                    i <- i + 1
+
+                while compare arr[j] pivot > 0 do
+                    j <- j - 1
+
+                if i <= j then
+                    swap arr i j
+                    i <- i + 1
+                    j <- j - 1
+
+            if left < j then
+                quick_sort arr left j
+
+            if i < right then
+                quick_sort arr i right
+
+
+    let rec merge (arr: 'a[]) (left: int ref) (mid: int ref) (right: int ref) =
+        let mutable start2 = mid.Value + 1
+
+        if compare arr.[mid.Value] arr.[start2] > 0 then
+            ()
+        else
+            while left.Value <= mid.Value && start2 <= right.Value do
+                if compare arr.[mid.Value] arr.[start2] > 0 then
+                    left.Value <- left.Value + 1
+                else
+                    let value = arr.[start2]
+                    let mutable index = start2
+
+                    while index > left.Value do
+                        swap arr index (index - 1)
+                        index <- index - 1
+
+                    arr.[left.Value] <- value
+
+                    left.Value <- left.Value + 1
+                    mid.Value <- mid.Value + 1
+                    start2 <- start2 + 1
+
+    let rec merge_sort (arr: 't[]) (left: int) (right: int) =
+        if left < right then
+            let mid = (right + left) / 2
+
+            merge_sort arr left mid
+            merge_sort arr (mid + 1) right
+
+            let leftRef = ref left
+            let midRef = ref mid
+            let rightRef = ref right
+
+            merge arr leftRef midRef rightRef
+
+
+module fibinacci =
+
+    type Matrix = int64 array array
+
+    let multiply (m1: Matrix) (m2: Matrix) : Matrix =
+        [| [| m1.[0].[0] * m2.[0].[0] + m1.[0].[1] * m2.[1].[0]
+              m1.[0].[0] * m2.[0].[1] + m1.[0].[1] * m2.[1].[1] |]
+           [| m1.[1].[0] * m2.[0].[0] + m1.[1].[1] * m2.[1].[0]
+              m1.[1].[0] * m2.[0].[1] + m1.[1].[1] * m2.[1].[1] |] |]
+
+    let rec power (matrix: Matrix) (n: int) : Matrix =
+        if n = 1 then
+            matrix
+        else
+            let halfPower = power matrix (n / 2)
+            let result = multiply halfPower halfPower
+            if n % 2 = 0 then result else multiply result matrix
+
+    let fibonacci (n: int) : int64 =
+        if n < 0 then
+            failwith "Fibonacci numbers for negative n are not defined\n"
+
+        if n = 0 then
+            0L
+        elif n = 1 then
+            1L
+        else
+            let mainMatrix = [| [| 1L; 1L |]; [| 1L; 0L |] |]
+            let resultMatrix = power mainMatrix (n - 1)
+            resultMatrix.[0].[0]
--- src/Tests/Tests.fs
+++ src/Tests/Tests.fs
@@ -3,9 +3,184 @@ namespace Tests
 open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
 
-[<TestClass>]
-type TestClass () =
+open Library.factorial
+open Library.sorts
+open Library.fibinacci
 
-    [<TestMethod>]
-    member this.TestMethodPassing () =
-        Assert.IsTrue(true);
+
+[<TestClass>]
+
+type factorial_tests() =
+
+    [<TestMethod>] // 0!
+    member this.TestFactorial_Zero() = Assert.AreEqual(1, factorial (0))
+
+    [<TestMethod>] // 1!
+    member this.TestFactorial_One() = Assert.AreEqual(1, factorial (1))
+
+    [<TestMethod>] // 5!
+    member this.TestFactorial_Five() = Assert.AreEqual(120, factorial (5))
+
+    [<TestMethod>] // (-1)!
+    member this.Factorial_negative() =
+        let ex = Assert.ThrowsException<System.Exception>(fun () -> factorial -1 :> obj)
+        Assert.AreEqual("The factorial of negative numbers is not calculated by this program\n", ex.Message)
+
+
+type bubble_sort_tests() =
+
+    [<TestMethod>] // empty array
+    member this.TestEmpty() =
+        let arr = [||]
+        bubble_sort arr
+        Assert.IsTrue(Seq.forall2 (=) arr [||])
+
+    [<TestMethod>] // sorted array
+    member this.TestSortedArray() =
+        let arr = [| 1; 2; 3; 4; 5 |]
+        bubble_sort arr
+        Assert.IsTrue(Seq.forall2 (=) arr [| 1; 2; 3; 4; 5 |])
+
+    [<TestMethod>] // reverse sorted array
+    member this.TestReverseSortedArray() =
+        let arr = [| 5; 4; 3; 2; 1 |]
+        bubble_sort arr
+        Assert.IsTrue(Seq.forall2 (=) arr [| 1; 2; 3; 4; 5 |])
+
+    [<TestMethod>] // unsorted array
+    member this.TestUnsortedArray() =
+        let arr = [| 3; 1; 4; 1; 5; 9; 2; 6; 5; 3; 5 |]
+        bubble_sort arr
+        Assert.IsTrue(Seq.forall2 (=) arr [| 1; 1; 2; 3; 3; 4; 5; 5; 5; 6; 9 |])
+
+    [<TestMethod>] // array with repeating elements
+    member this.TestArrayWithDuplicates() =
+        let arr = [| 4; 2; 2; 8; 3; 3; 1 |]
+        bubble_sort arr
+        Assert.IsTrue(Seq.forall2 (=) arr [| 1; 2; 2; 3; 3; 4; 8 |])
+
+    [<TestMethod>] // float array
+    member this.TestFloatArray() =
+        let arr = [| 3.14; 1.41; 2.71; 0.57; 4.67 |]
+        bubble_sort arr
+        Assert.IsTrue(Seq.forall2 (=) arr [| 0.57; 1.41; 2.71; 3.14; 4.67 |])
+
+    [<TestMethod>] // float array
+    member this.TestCharArray() =
+        let arr = [| 'D'; 'A'; 'C'; 'B'; 'E' |]
+        bubble_sort arr
+        Assert.IsTrue(Seq.forall2 (=) arr [| 'A'; 'B'; 'C'; 'D'; 'E' |])
+
+
+type quick_sort_tests() =
+
+    [<TestMethod>] // empty array
+    member this.Empty() =
+        let arr = [||]
+        quick_sort arr 0 0
+        Assert.IsTrue(Seq.forall2 (=) arr [||])
+
+    [<TestMethod>] // sorted array
+    member this.SortedArray() =
+        let arr = [| 1; 2; 3; 4; 5 |]
+        quick_sort arr 0 4
+        Assert.IsTrue(Seq.forall2 (=) arr [| 1; 2; 3; 4; 5 |])
+
+    [<TestMethod>] // reverse sorted array
+    member this.TestReverseSortedArray() =
+        let arr = [| 5; 4; 3; 2; 1 |]
+        quick_sort arr 0 4
+        Assert.IsTrue(Seq.forall2 (=) arr [| 1; 2; 3; 4; 5 |])
+
+    [<TestMethod>] // unsorted array
+    member this.TestUnsortedArray() =
+        let arr = [| 3; 1; 4; 1; 5; 9; 2; 6; 5; 3; 5 |]
+        quick_sort arr 0 10
+        Assert.IsTrue(Seq.forall2 (=) arr [| 1; 1; 2; 3; 3; 4; 5; 5; 5; 6; 9 |])
+
+    [<TestMethod>] // array with repeating
+    member this.TestArrayWithDuplicates() =
+        let arr = [| 4; 2; 2; 8; 3; 3; 1 |]
+        quick_sort arr 0 6
+        Assert.IsTrue(Seq.forall2 (=) arr [| 1; 2; 2; 3; 3; 4; 8 |])
+
+    [<TestMethod>] // float array
+    member this.TestFloatArray() =
+        let arr = [| 3.14; 1.41; 2.71; 0.57; 4.67 |]
+        quick_sort arr 0 4
+        Assert.IsTrue(Seq.forall2 (=) arr [| 0.57; 1.41; 2.71; 3.14; 4.67 |])
+
+    [<TestMethod>] // float array
+    member this.TestCharArray() =
+        let arr = [| 'D'; 'A'; 'C'; 'B'; 'E' |]
+        quick_sort arr 0 4
+        Assert.IsTrue(Seq.forall2 (=) arr [| 'A'; 'B'; 'C'; 'D'; 'E' |])
+
+
+type merge_sort_tests() =
+
+    [<TestMethod>] // empty array
+    member this.Empty() =
+        let arr = [||]
+        merge_sort arr 0 0
+        Assert.IsTrue(Seq.forall2 (=) arr [||])
+
+    [<TestMethod>] // sorted array
+    member this.SortedArray() =
+        let arr = [| 1; 2; 3; 4; 5 |]
+        merge_sort arr 0 4
+        Assert.IsTrue(Seq.forall2 (=) arr [| 1; 2; 3; 4; 5 |])
+
+    [<TestMethod>] // reverse sorted array
+    member this.TestReverseSortedArray() =
+        let arr = [| 5; 4; 3; 2; 1 |]
+        merge_sort arr 0 4
+        Assert.IsTrue(Seq.forall2 (=) arr [| 1; 2; 3; 4; 5 |])
+
+    [<TestMethod>] // unsorted array
+    member this.TestUnsortedArray() =
+        let arr = [| 3; 1; 4; 1; 5; 9; 2; 6; 5; 3; 5 |]
+        merge_sort arr 0 10
+        Assert.IsTrue(Seq.forall2 (=) arr [| 1; 1; 2; 3; 3; 4; 5; 5; 5; 6; 9 |])
+
+    [<TestMethod>] // array with repeating
+    member this.TestArrayWithDuplicates() =
+        let arr = [| 4; 2; 2; 8; 3; 3; 1 |]
+        merge_sort arr 0 6
+        Assert.IsTrue(Seq.forall2 (=) arr [| 1; 2; 2; 3; 3; 4; 8 |])
+
+    [<TestMethod>] // float array
+    member this.TestFloatArray() =
+        let arr = [| 3.14; 1.41; 2.71; 0.57; 4.67 |]
+        merge_sort arr 0 4
+        Assert.IsTrue(Seq.forall2 (=) arr [| 0.57; 1.41; 2.71; 3.14; 4.67 |])
+
+    [<TestMethod>] // float array
+    member this.TestCharArray() =
+        let arr = [| 'D'; 'A'; 'C'; 'B'; 'E' |]
+        merge_sort arr 0 4
+        Assert.IsTrue(Seq.forall2 (=) arr [| 'A'; 'B'; 'C'; 'D'; 'E' |])
+
+
+type fibinacci_matrix_tests() =
+
+    [<TestMethod>] // F0
+    member this.Fib_0() = Assert.AreEqual(0L, fibonacci (0))
+
+    [<TestMethod>] // F1
+    member this.Fib_1() = Assert.AreEqual(1L, fibonacci (1))
+
+    [<TestMethod>] // F2
+    member this.Fib_2() = Assert.AreEqual(1L, fibonacci (2))
+
+    [<TestMethod>] // F3
+    member this.Fib_3() = Assert.AreEqual(2L, fibonacci (3))
+
+    [<TestMethod>] // F30
+    member this.Fib_30() =
+        Assert.AreEqual(832040L, fibonacci (30))
+
+    [<TestMethod>] // F(-1)
+    member this.Fib_negative() =
+        let ex = Assert.ThrowsException<System.Exception>(fun () -> fibonacci -1 :> obj)
+        Assert.AreEqual("Fibonacci numbers for negative n are not defined\n", ex.Message)
