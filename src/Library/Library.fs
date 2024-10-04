5e98e434f1203d93c334a0497f880d4661ea5dcd
Kkkkateeee <kt.spbu@gmail.com>
2024-10-05 00:41:40 +0300

    fixed all sorts

--- src/Library/Library.fs
+++ src/Library/Library.fs
@@ -19,7 +19,7 @@ module sorts =
         arr[j] <- elem
 
 
-    let bubble_sort (arr: 't[]) =
+    let bubble_sort (arr: 't[]) : 't[] =
 
         let mutable array_len = arr.Length
         let mutable swapped = true
@@ -34,68 +34,66 @@ module sorts =
 
             array_len <- array_len - 1
 
-
-    let rec quick_sort (arr: 't[]) left right =
-
-        if left < right then
-            let pivot = arr[(left + right) / 2]
-            let mutable i = left
-            let mutable j = right
-
-            while i <= j do
-                while compare arr[i] pivot < 0 do
-                    i <- i + 1
-
-                while compare arr[j] pivot > 0 do
-                    j <- j - 1
-
-                if i <= j then
-                    swap arr i j
-                    i <- i + 1
-                    j <- j - 1
-
-            if left < j then
-                quick_sort arr left j
-
-            if i < right then
-                quick_sort arr i right
+        arr
 
 
-    let rec merge (arr: 'a[]) (left: int ref) (mid: int ref) (right: int ref) =
-        let mutable start2 = mid.Value + 1
+    let partition (arr: 't[]) (low: int) (high: int) =
+        let pivot = arr[high]
+        let mutable i = low - 1
 
-        if compare arr.[mid.Value] arr.[start2] > 0 then
-            ()
-        else
-            while left.Value <= mid.Value && start2 <= right.Value do
-                if compare arr.[mid.Value] arr.[start2] > 0 then
-                    left.Value <- left.Value + 1
+        for j in low .. (high - 1) do
+            if compare arr[j] pivot <= 0 then
+                i <- i + 1
+                swap arr i j
+
+        swap arr (i + 1) high
+        i + 1
+
+    let rec inplace_quick (arr: 't[]) (low: int) (high: int) =
+        if low < high then
+            let pivotIndex = partition arr low high
+            inplace_quick arr low (pivotIndex - 1)
+            inplace_quick arr (pivotIndex + 1) high
+
+    let quick_sort (arr: 't[]) : 't[] =
+        inplace_quick arr 0 (Array.length arr - 1)
+        arr
+
+
+    let merge (arr: 't[]) (low: byref<int>) (high: byref<int>) (mid: byref<int>) =
+        let mutable start2 = mid + 1
+
+        if compare arr[mid] arr[start2] > 0 then
+            while low <= mid && start2 <= high do
+                if compare arr[low] arr[start2] <= 0 then
+                    low <- low + 1
                 else
-                    let value = arr.[start2]
+                    let value = arr[start2]
                     let mutable index = start2
 
-                    while index > left.Value do
-                        swap arr index (index - 1)
+                    while index <> low do
+                        arr[index] <- arr[index - 1]
                         index <- index - 1
 
-                    arr.[left.Value] <- value
+                    arr[low] <- value
 
-                    left.Value <- left.Value + 1
-                    mid.Value <- mid.Value + 1
+                    low <- low + 1
+                    mid <- mid + 1
                     start2 <- start2 + 1
 
-    let rec merge_sort (arr: 't[]) (left: int) (right: int) =
-        if left < right then
-            let mid = (right + left) / 2
+    let rec inplace_merge (arr: 't[]) (low: byref<int>) (high: byref<int>) =
+        if low < high then
+            let mutable mid = (low + high) / 2
+            let mutable mid_plus_1 = mid + 1
+            inplace_merge arr &low &mid
+            inplace_merge arr &mid_plus_1 &high
+            merge arr &low &high &mid
 
-            merge_sort arr left mid
-            merge_sort arr (mid + 1) right
-
-            let leftRef = ref left
-            let midRef = ref mid
-            let rightRef = ref right
-
-            merge arr leftRef midRef rightRef
+    let merge_sort (arr: 't[]) : 't[] =
+        let mutable low = 0
+        let mutable high = Array.length arr - 1
+        inplace_merge arr &low &high
+        arr
 
 
 module fibinacci =
