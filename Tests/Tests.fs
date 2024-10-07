9c925bb857aa38944d7e2c683b89e196284cba1f
Kkkkateeee <kt.spbu@gmail.com>
2024-10-07 23:09:44 +0300

    made bubble_sort and tests for it

--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -1,69 +1,51 @@
-// namespace Tests
+namespace Tests
 
-// open System
-// open Microsoft.VisualStudio.TestTools.UnitTesting
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
 
-// open Library.factorial
-// open Library.sorts
-// open Library.fibinacci
+open Library.sorts
 
 
-// [<TestClass>]
-
-// type factorial_tests() =
-
-//     [<TestMethod>] // 0!
-//     member this.TestFactorial_Zero() = Assert.AreEqual(1, factorial (0))
-
-//     [<TestMethod>] // 1!
-//     member this.TestFactorial_One() = Assert.AreEqual(1, factorial (1))
-
-//     [<TestMethod>] // 5!
-//     member this.TestFactorial_Five() = Assert.AreEqual(120, factorial (5))
-
-//     [<TestMethod>] // (-1)!
-//     member this.TestFactorial_negative() =
-//         let ex = Assert.ThrowsException<System.Exception>(fun () -> factorial -1 :> obj)
-//         Assert.AreEqual("The factorial of negative numbers is not calculated by this program\n", ex.Message)
+[<TestClass>]
 
 
-// type sorts_test() =
-//     let int_test_cases =
-//         [| [||]
-//            [| 1; 2; 3; 4; 5 |]
-//            [| 5; 4; 3; 2; 1 |]
-//            [| 3; 1; 4; 1; 5; 9; 2; 6; 5; 3; 5 |]
-//            [| 4; 2; 2; 8; 3; 3; 1 |] |]
+type sorts_test() =
+    let int_test_cases =
+        [  []
+           [ 1; 2; 3; 4; 5 ]
+           [ 5; 4; 3; 2; 1 ]
+           [ 3; 1; 4; 1; 5; 9; 2; 6; 5; 3; 5 ]
+           [ 4; 2; 2; 8; 3; 3; 1 ] ]
 
-//     let float_test_cases = [| 3.14; 1.41; 2.71; 0.57; 4.67 |]
-//     let char_test_cases = [| 'D'; 'A'; 'C'; 'B'; 'E' |]
+    let float_test_cases = [ 3.14; 1.41; 2.71; 0.57; 4.67 ]
+    let char_test_cases = [ 'D'; 'A'; 'C'; 'B'; 'E' ]
 
-//     [<TestMethod>]
-//     member this.TestBubbleSort_int() =
-//         for elem in int_test_cases do
-//             let res = bubble_sort elem
-//             let expected = Array.sort elem
-//             Assert.AreEqual(expected, res)
+    [<TestMethod>]
+    member this.TestBubbleSort_int() =
+        for elem in int_test_cases do
+            let res = bubble_sort elem
+            let expected = List.sort elem
+            Assert.AreEqual(expected, res)
 
-//     [<TestMethod>]
-//     member this.TestBubbleSort_float() =
-//         let res = bubble_sort float_test_cases
-//         let expected = Array.sort float_test_cases
-//         Assert.AreEqual(expected, res)
+    [<TestMethod>]
+    member this.TestBubbleSort_float() =
+        let res = bubble_sort float_test_cases
+        let expected = List.sort float_test_cases
+        Assert.AreEqual(expected, res)
 
-//     [<TestMethod>]
-//     member this.TestBubbleSort_char() =
-//         let res = bubble_sort char_test_cases
-//         let expected = Array.sort char_test_cases
-//         Assert.AreEqual(expected, res)
+    [<TestMethod>]
+    member this.TestBubbleSort_char() =
+        let res = bubble_sort char_test_cases
+        let expected = List.sort char_test_cases
+        Assert.AreEqual(expected, res)
 
 
-//     [<TestMethod>]
-//     member this.TestQuickSort_int() =
-//         for elem in int_test_cases do
-//             let res = quick_sort elem
-//             let expected = Array.sort elem
-//             Assert.AreEqual(expected, res)
+    // [<TestMethod>]
+    // member this.TestQuickSort_int() =
+    //     for elem in int_test_cases do
+    //         let res = quick_sort elem
+    //         let expected = Array.sort elem
+    //         Assert.AreEqual(expected, res)
 
 //     [<TestMethod>]
 //     member this.TestQuickSort_float() =
--- src/Library/Library.fs
+++ src/Library/Library.fs
@@ -2,11 +2,19 @@
 
 
 module sorts =
-    let buble_sort list =
-        let buble list swap =
+     let rec bubble_sort (list: 't list) =
+        let rec bubble list =
             match list with 
-            | []
+            | [] -> []
             | [x] -> [x]
             | x1 :: x2 :: other ->
-                if x1 > x2 then
-                    x2 :: buble
+                if compare x1 x2 > 0 then 
+                    x2 :: bubble (x1 :: other)  
+                else
+                    x1 :: bubble (x2 :: other)  
+
+        let sorted_list = bubble list
+        if sorted_list = list then
+            sorted_list  
+        else
+            bubble_sort sorted_list  
