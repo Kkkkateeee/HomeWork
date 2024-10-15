476dc6db35e2813843843f6ca463d9a43d6a9e7b
Kkkkateeee <kt.spbu@gmail.com>
2024-10-15 20:17:59 +0300

    add more rantom tests

--- Tests/RandomTests.fs
+++ Tests/RandomTests.fs
@@ -9,20 +9,20 @@ open FsCheck.Arb
 type Overrides() =
     static member Float() =
         Arb.Default.Float()
-        |> filter (fun f -> not <| System.Double.IsNaN(f) &&
-                            not <| System.Double.IsInfinity(f)) 
-Arb.register<Overrides>() |> ignore
+        |> filter (fun f -> not <| System.Double.IsNaN(f) && not <| System.Double.IsInfinity(f))
+
+Arb.register<Overrides> () |> ignore
 
 
-[<Properties(MaxTest=10)>]
-type RandomTestsForBubleSort () =
+[<Properties(MaxTest = 10)>]
+type RandomTestsForBubleSort() =
 
     [<Property>]
-    member _.IntTest  (test_cases: int list) =
+    member _.IntTest(test_cases: int list) =
         let actual = bubble_sort (List_MyList test_cases) |> MyList_List
         let expected = List.sort test_cases
         Assert.True(expected.Length = actual.Length && List.forall2 (=) expected actual)
-    
+
     [<Property(Arbitrary = [| typeof<Overrides> |])>]
     member _.FloatTest(test_cases: float list) =
         let actual = bubble_sort (List_MyList test_cases) |> MyList_List
@@ -30,49 +30,117 @@ type RandomTestsForBubleSort () =
         Assert.True(expected.Length = actual.Length && List.forall2 (=) expected actual)
 
     [<Property>]
-    member _.CharTest  (test_cases: char list) =
+    member _.CharTest(test_cases: char list) =
         let actual = bubble_sort (List_MyList test_cases) |> MyList_List
         let expected = List.sort test_cases
         Assert.True(expected.Length = actual.Length && List.forall2 (=) expected actual)
 
 
-type RandomTestsForQuickSor1t () = 
+[<Properties(MaxTest = 10)>]
+type RandomTestsForQuickSor1t() =
 
     [<Property>]
-    member _.IntTest  (test_cases: int list) =
-        let actual =  quick_sort (List_MyList test_cases) |> MyList_List
+    member _.IntTest(test_cases: int list) =
+        let actual = quick_sort (List_MyList test_cases) |> MyList_List
         let expected = List.sort test_cases
         Assert.True(expected.Length = actual.Length && List.forall2 (=) expected actual)
-        
+
     [<Property(Arbitrary = [| typeof<Overrides> |])>]
-    member _.FloatTest  (test_cases: float list) =
+    member _.FloatTest(test_cases: float list) =
         let actual = quick_sort (List_MyList test_cases) |> MyList_List
         let expected = List.sort test_cases
         Assert.True(expected.Length = actual.Length && List.forall2 (=) expected actual)
 
     [<Property>]
-    member _.CharTest  (test_cases: char list) =
+    member _.CharTest(test_cases: char list) =
         let actual = quick_sort (List_MyList test_cases) |> MyList_List
         let expected = List.sort test_cases
         Assert.True(expected.Length = actual.Length && List.forall2 (=) expected actual)
 
 
-type RandomTestsForMergeSort () =
+[<Properties(MaxTest = 10)>]
+type RandomTestsForMergeSort() =
 
     [<Property>]
-    member _.IntTest  (test_cases: int list) =
-        let actual =  merge_sort (List_MyList test_cases) |> MyList_List
+    member _.IntTest(test_cases: int list) =
+        let actual = merge_sort (List_MyList test_cases) |> MyList_List
         let expected = List.sort test_cases
         Assert.True(expected.Length = actual.Length && List.forall2 (=) expected actual)
-        
+
     [<Property(Arbitrary = [| typeof<Overrides> |])>]
-    member _.FloatTest  (test_cases: float list) =
+    member _.FloatTest(test_cases: float list) =
         let actual = merge_sort (List_MyList test_cases) |> MyList_List
         let expected = List.sort test_cases
         Assert.True(expected.Length = actual.Length && List.forall2 (=) expected actual)
 
     [<Property>]
-    member _.CharTest  (test_cases: char list) =
+    member _.CharTest(test_cases: char list) =
         let actual = merge_sort (List_MyList test_cases) |> MyList_List
         let expected = List.sort test_cases
         Assert.True(expected.Length = actual.Length && List.forall2 (=) expected actual)
+
+
+[<Properties(MaxTest = 10)>]
+type BubbleEqualsQuick() =
+
+    [<Property>]
+    member _.IntTest(test_cases: int list) =
+        let bubble = bubble_sort (List_MyList test_cases) |> MyList_List
+        let quick = quick_sort (List_MyList test_cases) |> MyList_List
+        Assert.True(bubble.Length = quick.Length && List.forall2 (=) bubble quick)
+
+    [<Property(Arbitrary = [| typeof<Overrides> |])>]
+    member _.FloatTest(test_cases: float list) =
+        let bubble = bubble_sort (List_MyList test_cases) |> MyList_List
+        let quick = quick_sort (List_MyList test_cases) |> MyList_List
+        Assert.True(bubble.Length = quick.Length && List.forall2 (=) bubble quick)
+
+    [<Property>]
+    member _.CharTest(test_cases: char list) =
+        let bubble = bubble_sort (List_MyList test_cases) |> MyList_List
+        let quick = quick_sort (List_MyList test_cases) |> MyList_List
+        Assert.True(bubble.Length = quick.Length && List.forall2 (=) bubble quick)
+
+
+[<Properties(MaxTest = 10)>]
+type BubbleEqualsMerge() =
+
+    [<Property>]
+    member _.IntTest(test_cases: int list) =
+        let bubble = bubble_sort (List_MyList test_cases) |> MyList_List
+        let merge = merge_sort (List_MyList test_cases) |> MyList_List
+        Assert.True(bubble.Length = merge.Length && List.forall2 (=) bubble merge)
+
+    [<Property(Arbitrary = [| typeof<Overrides> |])>]
+    member _.FloatTest(test_cases: float list) =
+        let bubble = bubble_sort (List_MyList test_cases) |> MyList_List
+        let merge = merge_sort (List_MyList test_cases) |> MyList_List
+        Assert.True(bubble.Length = merge.Length && List.forall2 (=) bubble merge)
+
+    [<Property>]
+    member _.CharTest(test_cases: char list) =
+        let bubble = bubble_sort (List_MyList test_cases) |> MyList_List
+        let merge = merge_sort (List_MyList test_cases) |> MyList_List
+        Assert.True(bubble.Length = merge.Length && List.forall2 (=) bubble merge)
+
+
+[<Properties(MaxTest = 10)>]
+type MergeEqualsQuick() =
+
+    [<Property>]
+    member _.IntTest(test_cases: int list) =
+        let merge = bubble_sort (List_MyList test_cases) |> MyList_List
+        let quick = quick_sort (List_MyList test_cases) |> MyList_List
+        Assert.True(merge.Length = quick.Length && List.forall2 (=) merge quick)
+
+    [<Property(Arbitrary = [| typeof<Overrides> |])>]
+    member _.FloatTest(test_cases: float list) =
+        let merge = bubble_sort (List_MyList test_cases) |> MyList_List
+        let quick = quick_sort (List_MyList test_cases) |> MyList_List
+        Assert.True(merge.Length = quick.Length && List.forall2 (=) merge quick)
+
+    [<Property>]
+    member _.CharTest(test_cases: char list) =
+        let merge = bubble_sort (List_MyList test_cases) |> MyList_List
+        let quick = quick_sort (List_MyList test_cases) |> MyList_List
+        Assert.True(merge.Length = quick.Length && List.forall2 (=) merge quick)
--- src/Library/Library.fs
+++ src/Library/Library.fs
@@ -3,52 +3,54 @@
 
 module MyList =
 
-    type MyList<'t> = 
+    type MyList<'t> =
         | Empty
         | Cons of 't * MyList<'t>
 
     let rec List_MyList (list: 't List) : (MyList<'t>) =
-        match list with 
+        match list with
         | [] -> Empty
         | head :: tail -> Cons(head, List_MyList tail)
 
     let rec MyList_List (list: MyList<'t>) : ('t List) =
-        match list with 
+        match list with
         | Empty -> []
         | Cons(head, tail) -> head :: MyList_List tail
 
-    
-    
+
+
     let rec bubble_sort (list: MyList<'t>) : MyList<'t> =
         let rec bubble list =
-            match list with 
+            match list with
             | Empty -> Empty
             | Cons(x, Empty) -> Cons(x, Empty)
             | Cons(x1, Cons(x2, other)) ->
-                if compare x1 x2 > 0 then 
+                if compare x1 x2 > 0 then
                     Cons(x2, bubble (Cons(x1, other)))
                 else
-                    Cons(x1, bubble (Cons(x2, other))) 
+                    Cons(x1, bubble (Cons(x2, other)))
 
         let sorted_list = bubble list
-        if sorted_list = list then
-            sorted_list  
-        else
-            bubble_sort sorted_list  
 
-        
+        if sorted_list = list then
+            sorted_list
+        else
+            bubble_sort sorted_list
+
+
     let rec quick_sort (list: MyList<'t>) : MyList<'t> =
         match list with
         | Empty -> Empty
-        | Cons (pivot, other) ->
+        | Cons(pivot, other) ->
             let smaller, equal, larger = partition pivot other
             append (quick_sort smaller) (append equal (Cons(pivot, quick_sort larger)))
 
     and partition pivot list =
         match list with
         | Empty -> (Empty, Empty, Empty)
-        | Cons (head, tail) ->
+        | Cons(head, tail) ->
             let smaller, equal, larger = partition pivot tail
+
             if compare head pivot < 0 then
                 (Cons(head, smaller), equal, larger)
             elif head = pivot then
@@ -59,7 +61,7 @@ module MyList =
     and append xs ys =
         match xs with
         | Empty -> ys
-        | Cons (x, xs') -> Cons(x, append xs' ys)
+        | Cons(x, xs') -> Cons(x, append xs' ys)
 
 
     let rec merge_sort (list: MyList<'t>) : MyList<'t> =
@@ -68,7 +70,7 @@ module MyList =
         | Cons(x, Empty) -> Cons(x, Empty)
         | _ ->
             let left_list, right_list = separate list
-            merge (merge_sort left_list)(merge_sort right_list)
+            merge (merge_sort left_list) (merge_sort right_list)
 
     and merge (left_list: MyList<'t>) (right_list: MyList<'t>) =
         match left_list, right_list with
@@ -79,12 +81,11 @@ module MyList =
                 Cons(x, (merge xs right_list))
             else
                 Cons(y, (merge left_list ys))
-    
-    and separate (list: MyList<'t>) : (MyList<'t>* MyList<'t>) = 
+
+    and separate (list: MyList<'t>) : (MyList<'t> * MyList<'t>) =
         match list with
         | Empty -> (Empty, Empty)
         | Cons(x, Empty) -> (Cons(x, Empty), Empty)
         | Cons(x, Cons(y, other)) ->
             let (left_list, right_list) = separate other
             (Cons(x, left_list), Cons(y, right_list))
-            
