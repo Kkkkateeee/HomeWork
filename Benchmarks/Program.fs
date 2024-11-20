1d5e28e36ca0ebdd758fabf93adaabeb176e4bbc
Kkkkateeee <kt.spbu@gmail.com>
2024-11-20 03:34:40 +0300

    separated Mybenchmark to ListBenchmark and ArraysBenchmark

--- Benchmarks/Program.fs
+++ Benchmarks/Program.fs
@@ -5,29 +5,41 @@ open BenchmarkDotNet.Attributes
 open BenchmarkDotNet.Running
 open BenchmarkDotNet.Configs
 open BenchmarkDotNet.Reports
-open BenchmarkDotNet.Columns
-open BenchmarkDotNet.Environments
 open Perfolizer.Horology
 
 open ArraySorts
 open MyListSorts
 
+type ArraysBenchmark() =
 
-[<MemoryDiagnoser>]
-type MyBenchmark() =
-    
     static member Lengths = [|10000..10000..100000|]
     member this.Random = System.Random()
 
+    [<ParamsSource("Lengths")>]
+    member val ArrayLength = 0 with get, set
+    member val ArrayToSort = [|0.0|] with get, set
+
+    [<IterationSetup>]
+    member this.GetArrayToSort () = 
+        this.ArrayToSort <- Array.init this.ArrayLength (fun _ -> this.Random.NextDouble()) 
+
+    [<Benchmark>]
+    member this.ArrayQuickSort () = Arrays.quickSort this.ArrayToSort
+
+    [<Benchmark>]
+    member this.ArrayBubbleSort () = Arrays.bubbleSort this.ArrayToSort
+
+    [<Benchmark>]
+    member this.ArrayMergeSort () = Arrays.mergeSort this.ArrayToSort
+
+    [<Benchmark>]
+    member this.ArraySystem () = Array.Sort this.ArrayToSort
     
-    // [<ParamsSource("Lengths")>]
-    // member val ArrayLength = 0 with get, set
-    // member val ArrayToSort = [|0.0|] with get, set
 
-    // [<IterationSetup>]
-    // member this.GetArrayToSort () = 
-    //     this.ArrayToSort <- Array.init this.ArrayLength (fun _ -> this.Random.NextDouble()) 
-
+type ListBenchmark() =
+    
+    static member Lengths = [|10000..10000..100000|]
+    member this.Random = System.Random()
 
     [<ParamsSource("Lengths")>]
     member val ListLength = 0 with get, set
@@ -37,25 +49,11 @@ type MyBenchmark() =
     member this.GetListToSort () = 
         this.ListToSort <- List.init this.ListLength (fun _ -> this.Random.NextDouble()) 
 
-
-    // [<Benchmark>]
-    // member this.ArrayBubbleSort () = Arrays.bubbleSort this.ArrayToSort
-
-    // [<Benchmark>]
-    // member this.ArrayQuickSort () = Arrays.quickSort this.ArrayToSort
-
-    // [<Benchmark>]
-    // member this.ArrayMergeSort () = Arrays.mergeSort this.ArrayToSort
-
-    // [<Benchmark>]
-    // member this.ArraySystem () = Array.Sort this.ArrayToSort
-
+    [<Benchmark>]
+    member this.MyListBubbleSort () = MyList.fromSystemList this.ListToSort |> MyList.quickSort 
 
     [<Benchmark>]
-    member this.MyListBubbleSort () = MyList.fromSystemList this.ListToSort |> MyList.bubbleSort
-
-    [<Benchmark>]
-    member this.MyListQuickSort () = MyList.fromSystemList this.ListToSort |> MyList.quickSort 
+    member this.MyListQuickSort () = MyList.fromSystemList this.ListToSort |> MyList.bubbleSort
 
     [<Benchmark>]
     member this.MyListMergeSort () = MyList.fromSystemList this.ListToSort |> MyList.mergeSort
@@ -71,84 +69,8 @@ module Main =
         let config = ManualConfig.Create(DefaultConfig.Instance)
                         .WithSummaryStyle(SummaryStyle.Default.WithTimeUnit(TimeUnit.Millisecond)) 
                         .WithOptions(ConfigOptions.DisableOptimizationsValidator) 
-        
-        
-        BenchmarkRunner.Run<MyBenchmark>(config) |> ignore
-        0
-namespace Benchmarks
+        let benchmarks =
+            BenchmarkSwitcher [| typeof<ArraysBenchmark>;  typeof<ListBenchmark>|]
 
-open System
-open BenchmarkDotNet.Attributes
-open BenchmarkDotNet.Running
-open BenchmarkDotNet.Configs
-open BenchmarkDotNet.Reports
-open BenchmarkDotNet.Columns
-open BenchmarkDotNet.Environments
-open Perfolizer.Horology
-
-open ArraySorts
-open MyListSorts
-
-
-[<MemoryDiagnoser>]
-type MyBenchmark() =
-    
-    static member Lengths = [|10000..10000..100000|]
-    member this.Random = System.Random()
-
-    
-    // [<ParamsSource("Lengths")>]
-    // member val ArrayLength = 0 with get, set
-    // member val ArrayToSort = [|0.0|] with get, set
-
-    // [<IterationSetup>]
-    // member this.GetArrayToSort () = 
-    //     this.ArrayToSort <- Array.init this.ArrayLength (fun _ -> this.Random.NextDouble()) 
-
-
-    [<ParamsSource("Lengths")>]
-    member val ListLength = 0 with get, set
-    member val ListToSort = [0.0] with get, set
-
-    [<IterationSetup>]
-    member this.GetListToSort () = 
-        this.ListToSort <- List.init this.ListLength (fun _ -> this.Random.NextDouble()) 
-
-
-    // [<Benchmark>]
-    // member this.ArrayBubbleSort () = Arrays.bubbleSort this.ArrayToSort
-
-    // [<Benchmark>]
-    // member this.ArrayQuickSort () = Arrays.quickSort this.ArrayToSort
-
-    // [<Benchmark>]
-    // member this.ArrayMergeSort () = Arrays.mergeSort this.ArrayToSort
-
-    // [<Benchmark>]
-    // member this.ArraySystem () = Array.Sort this.ArrayToSort
-
-
-    [<Benchmark>]
-    member this.MyListBubbleSort () = MyList.fromSystemList this.ListToSort |> MyList.bubbleSort
-
-    [<Benchmark>]
-    member this.MyListQuickSort () = MyList.fromSystemList this.ListToSort |> MyList.quickSort 
-
-    [<Benchmark>]
-    member this.MyListMergeSort () = MyList.fromSystemList this.ListToSort |> MyList.mergeSort
-
-    [<Benchmark>]
-    member this.MyListSystem () = List.sort this.ListToSort 
-
-
-module Main =
-    [<EntryPoint>]
-    let main argv =
-        
-        let config = ManualConfig.Create(DefaultConfig.Instance)
-                        .WithSummaryStyle(SummaryStyle.Default.WithTimeUnit(TimeUnit.Millisecond)) 
-                        .WithOptions(ConfigOptions.DisableOptimizationsValidator) 
-        
-        
-        BenchmarkRunner.Run<MyBenchmark>(config) |> ignore
+        benchmarks.Run argv |> ignore
         0
