3f07477456121f0ce0a8ad4d6bfcbed5324c10da
Kate Titova <kt.spbu@gmail.com>
2024-12-06 04:17:43 +0500

    Done 7 homeworks
    Rename 5.pdf to presentation.pdf
    
    merged HW6 to master
    
    * made Trees funcs
    
    * made empty tests files
    
    * made tests
    
    * changed all according pull request
    
    * char << ((+) 1 << int)
    
    divided all test files into folders
    
    HomeWork_5 (#14)
    
    * HW5
    
    fixed all according pull request
    
    enum filters
    
    help
    
    rebased HW5 into master
    
    sa
    
    s
    
    * couldnt fix parametr for generators
    
    + HomeWork 7
    
    * done
    
    * made structure Matrix, removed size from QTrees
    
    * removed unnecessary data
    
    * fixed :)
    
    * removed cached
    
    edit
    
    a
    
    ghg
    
    a

---
+++ src/ImageProcessing/Program.fs
@@ -0,0 +1,72 @@
+﻿open ImageProcessing.ImProcessing
+open Argu
+
+
+type Filters =
+    | gaussianBlur = 1
+    | motionDiagonal135deg = 2  
+    | motionDiagonal315deg = 3
+    | motionVertical = 4
+    | motionHorizontal = 5
+    | edgesHorizontal = 6
+    | edgesVertical = 7
+    | edgesDioganal135deg = 8
+    | edgesDioganal315deg = 9
+    | edgesAllDirections = 10
+    | sharpen = 11
+    | sharpenSoft = 12
+    | sharpenWithEdges = 13
+    | emboss = 14
+    | embossHard = 15
+
+type Arguments =
+    | [<Mandatory>] Input_File of string
+    | [<Mandatory>] Out_File of string
+    | [<Mandatory>] Filters of list<Filters>
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with 
+            | Input_File _ -> "File to process."
+            | Out_File _ -> "Where to save result."
+            | Filters _ -> "Which filters to apply (comma-separated)."
+
+
+[<EntryPoint>]
+let main argv = 
+    
+    let parser = ArgumentParser.Create<Arguments>(programName = "ImageProcessing")  
+    let results = parser.Parse argv  
+
+    let inFile = results.GetResult Input_File  
+    let outFile = results.GetResult Out_File
+    let filters = results.GetResult Filters
+
+    let inImage = loadAsRgba2D inFile 
+
+    let resultImage =
+        filters |> List.fold (fun img filter ->
+            match filter with 
+            | Filters.gaussianBlur -> applyFilter gaussianBlur img 
+            | Filters.motionDiagonal135deg -> applyFilter motionDiagonal135deg img 
+            | Filters.motionDiagonal315deg -> applyFilter motionDiagonal315deg img 
+            | Filters.motionVertical -> applyFilter motionVertical img 
+            | Filters.motionHorizontal -> applyFilter motionHorizontal img 
+            | Filters.edgesHorizontal -> applyFilter edgesHorizontal img 
+            | Filters.edgesVertical -> applyFilter edgesVertical img 
+            | Filters.edgesDioganal135deg -> applyFilter edgesDioganal135deg img 
+            | Filters.edgesDioganal315deg -> applyFilter edgesDioganal315deg img 
+            | Filters.edgesAllDirections -> applyFilter edgesAllDirections img 
+            | Filters.sharpen -> applyFilter sharpen img 
+            | Filters.sharpenSoft -> applyFilter sharpenSoft img 
+            | Filters.sharpenWithEdges -> applyFilter sharpenWithEdges img 
+            | Filters.emboss -> applyFilter emboss img 
+            | Filters.embossHard -> applyFilter embossHard img 
+            | _ -> 
+                printfn "Unknown filter"
+                img
+                
+        ) inImage
+    
+    saveRgbaImage resultImage outFile
+    
+    0
---
+++ src/Trees/Trees.fs
@@ -0,0 +1,66 @@
+﻿namespace Trees
+
+type NonEmptyList<'t> =
+    | Single of 't
+    | Cons of 't * NonEmptyList<'t>
+
+type Tree<'t> =
+    | Node of Tree<'t> NonEmptyList
+    | Leaf of 't
+
+
+module NonEmptyList = 
+
+    let rec map func list =
+        match list with 
+        | Single value -> Single (func value)
+        | Cons(head, tail) -> Cons(func head, map func tail)
+
+    let rec fold func acc list =
+        match list with 
+        | Single value -> func value acc
+        | Cons(head, tail) -> 
+            let newAcc = func head acc
+            fold func newAcc tail
+
+    let rec foldBack func list acc =
+        match list with 
+        | Single value -> func value acc
+        | Cons(head, tail) -> foldBack func tail (func head acc)
+
+    let rec max list =
+        match list with 
+        | Single x -> x
+        | Cons (head, tail) ->
+            let tailMax = max tail
+            if head > tailMax then head else tailMax
+
+
+module Tree =
+
+    let rec map func tree  = 
+        match tree with 
+        | Leaf value -> Leaf (func value)
+        | Node children ->
+            Node (NonEmptyList.map (map func) children)
+
+    let rec foldLeft func acc tree =
+        match tree with 
+        | Leaf value -> func acc value
+        | Node children -> 
+            NonEmptyList.fold (fun child acc -> foldLeft func acc child) acc children
+
+    let rec foldRight func acc tree =
+        match tree with 
+        | Leaf value -> func acc value
+        | Node children ->
+            NonEmptyList.foldBack (fun child acc -> foldRight func acc child) children acc 
+
+    let rec high tree=
+        match tree with
+        | Leaf _ -> 1
+        | Node children -> 
+            let heights = NonEmptyList.map high children
+            1 + NonEmptyList.max heights
+
+    
