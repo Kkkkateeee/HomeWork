1ef5d1798a83ed045c61eabbe9a07e337e932bed
Kkkkateeee <kt.spbu@gmail.com>
2025-08-11 15:37:11 +0500

    finished

--- Tests/ImageProcessingT/PropertyImageProcessing.fs
+++ Tests/ImageProcessingT/PropertyImageProcessing.fs
@@ -18,12 +18,13 @@ module ImageGen =
             let a = byte 255
             return Rgba32(r, g, b, a)
         }
-    let array2DGen (size: int) : Gen<Rgba32[,]> =
+    let array2DGen (size: int) : Gen<Async<Rgba32[, ]>> = 
         gen {
             let! pixels = Gen.array2DOfDim (size, size) pixelGen
-            return pixels
+            return async { return pixels }
         }
-    
+
+
 type Image100() =
     static member Rgba32() =
         Arb.fromGen (ImageGen.array2DGen 100)
@@ -37,40 +38,149 @@ type Image2() =
 type Filter() =
 
     [<Property(Arbitrary = [| typeof<Image100> |])>]
-    member _.filterDoesntChangeSize (image: Rgba32[,]) =
-        let imageAfterFilter = applyFilter gaussianBlur image
-        Assert.Equal(100, imageAfterFilter.GetLength(0))
-        Assert.Equal(100, imageAfterFilter.GetLength(1))
+    member _.filterDoesntChangeSize (image: Async<Rgba32[, ]>) =
+        let resNoParallelism = 
+            applyFilterNoParallelismA gaussianBlur image
+            |> Async.RunSynchronously
+
+        let resPixelParallelism = 
+            applyFilterPixelParallelismA gaussianBlur image
+            |> Async.RunSynchronously
+
+        let resPartsParallelism = 
+            applyFilterParallelismInPartsA gaussianBlur image
+            |> Async.RunSynchronously
+
+        let resRowParallelism = 
+            applyFilterRowParallelismA gaussianBlur image
+            |> Async.RunSynchronously
+
+        let resColParallelism = 
+            applyFilterColParallelismA gaussianBlur image
+            |> Async.RunSynchronously
+
+        Assert.Equal(100, resNoParallelism.GetLength 0)
+        Assert.Equal(100, resNoParallelism.GetLength 1)
+
+        Assert.Equal(100, resPixelParallelism.GetLength 0)
+        Assert.Equal(100, resPixelParallelism.GetLength 1)
+
+        Assert.Equal(100, resPartsParallelism.GetLength 0)
+        Assert.Equal(100, resPartsParallelism.GetLength 1)
+
+        Assert.Equal(100, resRowParallelism.GetLength 0)
+        Assert.Equal(100, resRowParallelism.GetLength 1)
+
+        Assert.Equal(100, resColParallelism.GetLength 0)
+        Assert.Equal(100, resColParallelism.GetLength 1)
 
     [<Property(Arbitrary = [| typeof<Image100> |])>]
-    member _.idDoesntChangeData (image: Rgba32[,]) =
-        let imageAfterFilter = applyFilter id image
-        Assert.Equal(image, imageAfterFilter)
+    member _.idDoesntChangeData (image: Async<Rgba32[, ]>) =
+        let resNoParallelism = applyFilterNoParallelismA id image
+        let resPixelParallelism = applyFilterPixelParallelismA id image
+        let resPartsParallelism = applyFilterParallelismInPartsA id image
+        let resRowParallelism = applyFilterRowParallelismA id image
+        let resColParallelism = applyFilterColParallelismA id image
+
+        Assert.Equal(image |> Async.RunSynchronously, resNoParallelism |> Async.RunSynchronously)
+        Assert.Equal(image |> Async.RunSynchronously, resPixelParallelism |> Async.RunSynchronously)
+        Assert.Equal(image |> Async.RunSynchronously, resPartsParallelism |> Async.RunSynchronously)
+        Assert.Equal(image |> Async.RunSynchronously, resRowParallelism |> Async.RunSynchronously)
+        Assert.Equal(image |> Async.RunSynchronously, resColParallelism |> Async.RunSynchronously)
 
     [<Property(Arbitrary = [| typeof<Image2> |])>]
-    member _.imageSmallerThanFilter (image: Rgba32[,]) =
-        let imageAfterFilter = applyFilter black image
-        Assert.Equal(true, imageIsBlack imageAfterFilter)
+    member _.imageSmallerThanFilter (image: Async<Rgba32[, ]>) =
+        let resNoParallelism = applyFilterNoParallelismA black image
+        let resPixelParallelism = applyFilterPixelParallelismA black image
+        let resPartsParallelism = applyFilterParallelismInPartsA black image
+        let resRowParallelism = applyFilterRowParallelismA black image
+        let resColParallelism = applyFilterColParallelismA black image
+        
+        Assert.Equal(true, imageIsBlack resNoParallelism)
+        Assert.Equal(true, imageIsBlack resPixelParallelism)
+        Assert.Equal(true, imageIsBlack resPartsParallelism)
+        Assert.Equal(true, imageIsBlack resRowParallelism)
+        Assert.Equal(true, imageIsBlack resColParallelism)
 
     [<Property(Arbitrary = [| typeof<Image100> |])>]
-    member _.blackFilter (image: Rgba32[,]) =
-        let imageBlack = applyFilter black image
-        Assert.Equal(true, imageIsBlack imageBlack)
+    member _.blackFilter (image: Async<Rgba32[, ]>) =
+        let imageBlack_NoParallelism = applyFilterNoParallelismA black image
+        let imageBlack_PixelParallelism = applyFilterPixelParallelismA black image
+        let imageBlack_PartsParallelism = applyFilterParallelismInPartsA black image
+        let imageBlac_RowParallelism = applyFilterRowParallelismA black image
+        let imageBlack_ColParallelism = applyFilterColParallelismA black image
+        
+        Assert.Equal(true, imageIsBlack imageBlack_NoParallelism)
+        Assert.Equal(true, imageIsBlack imageBlack_PixelParallelism)
+        Assert.Equal(true, imageIsBlack imageBlack_PartsParallelism)
+        Assert.Equal(true, imageIsBlack imageBlac_RowParallelism)
+        Assert.Equal(true, imageIsBlack imageBlack_ColParallelism)
 
     [<Property(Arbitrary = [| typeof<Image100> |])>]
-    member _.shiftComposition (image: Rgba32[,]) =
-        let trivial1and2 = applyFilter shiftRight image |> applyFilter shiftDown
-        let trivial12 = applyFilter shiftDiagonal image
-        Assert.Equal(trivial1and2, trivial12)
+    member _.shiftComposition (image: Async<Rgba32[, ]>) =
+        let trivial1and2_NoParallelism = applyFilterNoParallelismA shiftRight image |> applyFilterNoParallelismA shiftDown
+        let trivial12_NoParallelism = applyFilterNoParallelismA shiftDiagonal image
+
+        let trivial1and2_PixelParallelism = applyFilterPixelParallelismA shiftRight image |> applyFilterPixelParallelismA shiftDown
+        let trivial12_PixelParallelism = applyFilterPixelParallelismA shiftDiagonal image
+
+        let trivial1and2_PartsParallelism = applyFilterParallelismInPartsA shiftRight image |> applyFilterParallelismInPartsA shiftDown
+        let trivial12_PartsParallelism = applyFilterParallelismInPartsA shiftDiagonal image
+
+        let trivial1and2_RowParallelism = applyFilterRowParallelismA shiftRight image |> applyFilterRowParallelismA shiftDown
+        let trivial12_RowParallelism = applyFilterRowParallelismA shiftDiagonal image
+
+        let trivial1and2_ColParallelism = applyFilterColParallelismA shiftRight image |> applyFilterColParallelismA shiftDown
+        let trivial12_ColParallelism = applyFilterColParallelismA shiftDiagonal image
+
+        Assert.Equal(trivial1and2_NoParallelism |> Async.RunSynchronously, trivial12_NoParallelism |> Async.RunSynchronously)
+        Assert.Equal(trivial1and2_PixelParallelism |> Async.RunSynchronously, trivial12_PixelParallelism |> Async.RunSynchronously)
+        Assert.Equal(trivial1and2_PartsParallelism |> Async.RunSynchronously, trivial12_PartsParallelism |> Async.RunSynchronously)
+        Assert.Equal(trivial1and2_RowParallelism |> Async.RunSynchronously, trivial12_RowParallelism |> Async.RunSynchronously)
+        Assert.Equal(trivial1and2_ColParallelism |> Async.RunSynchronously, trivial12_ColParallelism |> Async.RunSynchronously)
 
     [<Property(Arbitrary = [| typeof<Image100> |])>]
-    member _.extendedComposition (image: Rgba32[,])  =
-        let imageAfterFilter = applyFilter kernel image
-        let imageAfterExtendedFilter = applyFilter kernelExtended image
-        Assert.Equal(imageAfterFilter, imageAfterExtendedFilter)
+    member _.extendedComposition (image: Async<Rgba32[, ]>)  =
+        let imageAfterFilter_NoParallelism = applyFilterNoParallelismA kernel image 
+        let imageAfterExtendedFilter_NoParallelism = applyFilterNoParallelismA kernelExtended image
+
+        let imageAfterFilter_PixelParallelism = applyFilterPixelParallelismA kernel image
+        let imageAfterExtendedFilter_PixelParallelism = applyFilterPixelParallelismA kernelExtended image
+
+        let imageAfterFilter_PartsParallelism = applyFilterParallelismInPartsA kernel image
+        let imageAfterExtendedFilter_PartsParallelism = applyFilterParallelismInPartsA kernelExtended image
+
+        let imageAfterFilter_RowParallelism = applyFilterRowParallelismA kernel image
+        let imageAfterExtendedFilter_RowParallelism = applyFilterRowParallelismA kernelExtended image
+
+        let imageAfterFilter_ColParallelism = applyFilterColParallelismA kernel image
+        let imageAfterExtendedFilter_ColParallelism = applyFilterColParallelismA kernelExtended image
+
+        Assert.Equal(imageAfterFilter_NoParallelism |> Async.RunSynchronously, imageAfterExtendedFilter_NoParallelism |> Async.RunSynchronously)
+        Assert.Equal(imageAfterFilter_PixelParallelism |> Async.RunSynchronously, imageAfterExtendedFilter_PixelParallelism |> Async.RunSynchronously)
+        Assert.Equal(imageAfterFilter_PartsParallelism |> Async.RunSynchronously, imageAfterExtendedFilter_PartsParallelism |> Async.RunSynchronously)
+        Assert.Equal(imageAfterFilter_RowParallelism |> Async.RunSynchronously, imageAfterExtendedFilter_RowParallelism |> Async.RunSynchronously)
+        Assert.Equal(imageAfterFilter_ColParallelism |> Async.RunSynchronously, imageAfterExtendedFilter_ColParallelism |> Async.RunSynchronously)
 
     [<Property(Arbitrary = [| typeof<Image100> |])>]
-    member _.someAreCommutative (image: Rgba32[,]) =
-        let image12 = applyFilter shiftRight image |> applyFilter shiftDown
-        let image21 = applyFilter shiftDown image |> applyFilter shiftRight
-        Assert.Equal(image12, image21)
+    member _.someAreCommutative (image: Async<Rgba32[, ]>) =
+        let image12_NoParallelism = applyFilterNoParallelismA shiftRight image |> applyFilterNoParallelismA shiftDown
+        let image21_NoParallelism = applyFilterNoParallelismA shiftDown image |> applyFilterNoParallelismA shiftRight
+
+        let image12_PixelParallelism = applyFilterPixelParallelismA shiftRight image |> applyFilterPixelParallelismA shiftDown
+        let image21_PixelParallelism = applyFilterPixelParallelismA shiftDown image |> applyFilterPixelParallelismA shiftRight
+
+        let image12_PartsParallelism = applyFilterParallelismInPartsA shiftRight image |> applyFilterParallelismInPartsA shiftDown
+        let image21_PartsParallelism = applyFilterParallelismInPartsA shiftDown image |> applyFilterParallelismInPartsA shiftRight
+
+        let image12_RowParallelism = applyFilterRowParallelismA shiftRight image |> applyFilterRowParallelismA shiftDown
+        let image21_RowParallelism = applyFilterRowParallelismA shiftDown image |> applyFilterRowParallelismA shiftRight
+
+        let image12_ColParallelism = applyFilterColParallelismA shiftRight image |> applyFilterColParallelismA shiftDown
+        let image21_ColParallelism = applyFilterColParallelismA shiftDown image |> applyFilterColParallelismA shiftRight
+        
+        Assert.Equal(image12_NoParallelism |> Async.RunSynchronously, image21_NoParallelism |> Async.RunSynchronously)
+        Assert.Equal(image12_PixelParallelism |> Async.RunSynchronously, image21_PixelParallelism |> Async.RunSynchronously)
+        Assert.Equal(image12_PartsParallelism |> Async.RunSynchronously, image21_PartsParallelism |> Async.RunSynchronously)
+        Assert.Equal(image12_RowParallelism |> Async.RunSynchronously, image21_RowParallelism |> Async.RunSynchronously)
+        Assert.Equal(image12_ColParallelism |> Async.RunSynchronously, image21_ColParallelism |> Async.RunSynchronously)
--- src/ImageProcessing/ImageProcessing.fs
+++ src/ImageProcessing/ImageProcessing.fs
@@ -2,6 +2,9 @@ namespace ImageProcessing
 
 open SixLabors.ImageSharp
 open SixLabors.ImageSharp.PixelFormats
+open System.Threading.Tasks
+open Microsoft.FSharp.Control
+open System.IO
 
 [<Struct>]
 type Image =
@@ -183,6 +186,193 @@ module ImProcessing =
         |> Array.map (Array.map (fun x -> (float32 x) / + 128.0f))
 
 
+    let loadAsRgba2DA (file: string) =
+        async {
+            use fileStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read, bufferSize = 4096, useAsync = true) 
+
+            let! img = Image.LoadAsync<Rgba32> fileStream |> Async.AwaitTask 
+            let res = Array2D.zeroCreate<Rgba32> img.Height img.Width
+
+            for i in 0 .. img.Width - 1 do
+                for j in 0 .. img.Height - 1 do
+                    res.[j, i] <- img.[i, j]
+
+            return res
+        } 
+        
+    let saveRgbaImageA (rgbaDataAsync: Async<Rgba32[,]>) file =
+        async {
+            let! rgbaData = rgbaDataAsync
+            
+            let h = rgbaData.GetLength 0
+            let w = rgbaData.GetLength 1
+
+            use img = new Image<Rgba32>(w, h)
+
+            for x in 0 .. h - 1 do
+                for y in 0 .. w - 1 do
+                    img.[y, x] <- rgbaData.[x, y] 
+
+            let! _ =  img.SaveAsync file |> Async.AwaitTask
+            return ()
+        }
+
+    let processPixel px py (filter: float32 array) filterD (img: Rgba32[,]) imgH imgW =
+        let dataToHandle = [| 
+            for i in px - filterD .. px + filterD do
+                for j in py - filterD .. py + filterD do
+                    if i < 0 || i >= imgH || j < 0 || j >= imgW then
+                        0.0f, 0.0f, 0.0f
+                    else
+                        let pixel = img.[i, j]
+                        float32 pixel.R, float32 pixel.G, float32 pixel.B
+        |]
+
+        let mutable rSum = 0.0f
+        let mutable gSum = 0.0f
+        let mutable bSum = 0.0f
+
+        Array.iteri (fun index (r, g, b) ->
+            rSum <- rSum + r * filter.[index]
+            gSum <- gSum + g * filter.[index]
+            bSum <- bSum + b * filter.[index]
+        ) dataToHandle
+
+        byte rSum, byte gSum, byte bSum // Async<[,]<Rgba32>>
+
+    let applyFilterNoParallelismA (filter: float32[][]) (imgAsync: Async<Rgba32[,]>) =
+        async {
+            let! img = imgAsync
+
+            let imgH = img.GetLength 0
+            let imgW = img.GetLength 1
+
+            let filterD = Array.length filter / 2
+            let filter = Array.concat filter
+
+            let res =
+                Array2D.mapi (fun x y _ ->
+                    let r, g, b = processPixel x y filter filterD img imgH imgW
+                    Rgba32(r, g, b, img.[x, y].A) 
+                ) img
+            return res
+        }
+
+    let applyFilterPixelParallelismA (filter: float32[][]) (imgAsync: Async<Rgba32[,]>) =
+        async {
+            let! img = imgAsync  
+
+            let imgH = img.GetLength 0
+            let imgW = img.GetLength 1
+
+            let filterD = Array.length filter / 2
+            let filter = Array.concat filter
+
+            let res = Array2D.create imgH imgW (Rgba32(0.0f, 0.0f, 0.0f, 0.0f))
+
+            Parallel.For(0, imgH * imgW, fun k ->
+                let i = k / imgW
+                let j = k % imgW  
+
+                let r, g, b = processPixel i j filter filterD img imgH imgW
+                res.[i, j] <- Rgba32(r, g, b, img.[i, j].A)
+
+            ) |> ignore
+
+            return res
+        }
+
+    let applyFilterParallelismInPartsA (filter: float32[][]) (imgAsync: Async<Rgba32[,]>) =
+        async {
+            let! img = imgAsync
+
+            let imgH = img.GetLength 0
+            let imgW = img.GetLength 1
+
+            let filterD = Array.length filter / 2
+            let filter = Array.concat filter
+
+            let halfImgH = imgH / 2
+            let halfImgW = imgW / 2
+
+            let res = Array2D.create imgH imgW (Rgba32(0.0f, 0.0f, 0.0f, 0.0f))
+
+            Parallel.For (0, 4, fun k -> 
+                if k = 0 then 
+                    for i in 0 .. halfImgH - 1 do 
+                        for j in 0 .. halfImgW - 1 do 
+                            let r, g, b = processPixel i j filter filterD img imgH imgW
+                            res.[i, j] <- Rgba32(r, g, b, img.[i, j].A)
+
+                elif k = 1 then 
+                    for i in halfImgH .. imgH - 1 do 
+                        for j in 0 .. halfImgW - 1 do 
+                            let r, g, b = processPixel i j filter filterD img imgH imgW
+                            res.[i, j] <- Rgba32(r, g, b, img.[i, j].A)                
+                
+                elif k = 2 then 
+                    for i in 0 .. halfImgH - 1 do 
+                        for j in halfImgW .. imgW - 1 do 
+                            let r, g, b = processPixel i j filter filterD img imgH imgW
+                            res.[i, j] <- Rgba32(r, g, b, img.[i, j].A)   
+
+                else 
+                    for i in halfImgH .. imgH - 1 do 
+                        for j in halfImgW .. imgW - 1 do 
+                            let r, g, b = processPixel i j filter filterD img imgH imgW
+                            res.[i, j] <- Rgba32(r, g, b, img.[i, j].A)  
+            ) |> ignore
+            
+            return res
+        }
+
+    let applyFilterRowParallelismA (filter: float32[][]) (imgAsync: Async<Rgba32[,]>) =
+        async {
+            let! img = imgAsync
+
+            let imgH = img.GetLength 0
+            let imgW = img.GetLength 1
+
+            let filterD = Array.length filter / 2
+            let filter = Array.concat filter
+
+            let res = Array2D.create imgH imgW (Rgba32(0.0f, 0.0f, 0.0f, 0.0f))
+
+            Parallel.For (0, imgH, fun k -> 
+                for i in 0 .. imgW - 1 do 
+                    let r, g, b = processPixel k i filter filterD img imgH imgW
+                    res.[k, i] <- Rgba32(r, g, b, img.[k, i].A)      
+            ) |> ignore
+
+            return res
+        }
+
+    let applyFilterColParallelismA (filter: float32[][]) (imgAsync: Async<Rgba32[,]>) =
+        async {
+            let! img = imgAsync
+
+            let imgH = img.GetLength 0
+            let imgW = img.GetLength 1
+
+            let filterD = Array.length filter / 2
+            let filter = Array.concat filter
+
+            let res = Array2D.create imgH imgW (Rgba32(0.0f, 0.0f, 0.0f, 0.0f))
+
+            Parallel.For (0, imgW, fun k -> 
+                for i in 0 .. imgH - 1 do 
+                    let r, g, b = processPixel i k filter filterD img imgH imgW
+                    res.[i, k] <- Rgba32(r, g, b, img.[i, k].A)      
+            ) |> ignore
+
+            return res
+        }
+
+
+
+// СИНХРОННАЯ ВЕРСИЯ ФУНКЦИЙ ВЫШЕ
+
+
     let loadAsRgba2D (file: string) =
         let img = Image.Load<Rgba32> file
         let res = Array2D.zeroCreate<Rgba32> img.Height img.Width
@@ -190,12 +380,11 @@ module ImProcessing =
         for i in 0 .. img.Width - 1 do
             for j in 0 .. img.Height - 1 do
                 res.[j, i] <- img.[i, j]
-
         res
 
     let saveRgbaImage (rgbaData: Rgba32[,]) file =
-        let h = rgbaData.GetLength(0)
-        let w = rgbaData.GetLength(1)
+        let h = rgbaData.GetLength 0
+        let w = rgbaData.GetLength 1
 
         use img = new Image<Rgba32>(w, h)
 
@@ -203,39 +392,106 @@ module ImProcessing =
             for y in 0 .. w - 1 do
                 img.[y, x] <- rgbaData.[x, y] 
 
-        img.Save(file)
+        img.Save file 
 
-    let applyFilter (filter: float32[][]) (img: Rgba32[,]) =
+    let applyFilterNoParallelism (filter: float32[][]) (img: Rgba32[,]) =
         let imgH = img.GetLength 0
         let imgW = img.GetLength 1
 
-        let filterD = (Array.length filter) / 2
+        let filterD = Array.length filter / 2
         let filter = Array.concat filter
 
-        let processPixel px py =
-            let dataToHandle = [| 
-                for i in px - filterD .. px + filterD do
-                    for j in py - filterD .. py + filterD do
-                        if i < 0 || i >= imgH || j < 0 || j >= imgW then
-                            (0.0f, 0.0f, 0.0f) 
-                        else
-                            let pixel = img.[i, j]
-                            (float32 pixel.R, float32 pixel.G, float32 pixel.B)
-            |]
-
-            let mutable rSum = 0.0f
-            let mutable gSum = 0.0f
-            let mutable bSum = 0.0f
-
-            Array.iteri (fun index (r, g, b) ->
-                rSum <- rSum + r * filter.[index]
-                gSum <- gSum + g * filter.[index]
-                bSum <- bSum + b * filter.[index]
-            ) dataToHandle
-
-            (byte rSum, byte gSum, byte bSum)
-
         Array2D.mapi (fun x y _ ->
-            let (r, g, b) = processPixel x y
+            let r, g, b = processPixel x y filter filterD img imgH imgW
             Rgba32(r, g, b, img.[x, y].A) 
         ) img
+
+    let applyFilterPixelParallelism (filter: float32[][]) (img: Rgba32[,]) =
+        let imgH = img.GetLength 0
+        let imgW = img.GetLength 1
+
+        let filterD = Array.length filter / 2
+        let filter = Array.concat filter
+
+        let res = Array2D.create imgH imgW (Rgba32(0.0f, 0.0f, 0.0f, 0.0f))
+
+        Parallel.For(0, imgH * imgW, fun k ->
+            let i = k / imgW
+            let j = k % imgW  
+
+            let r, g, b = processPixel i j filter filterD img imgH imgW
+            res.[i, j] <- Rgba32(r, g, b, img.[i, j].A)
+
+        ) |> ignore
+        res 
+
+    let applyFilterParallelismInParts (filter: float32[][]) (img: Rgba32[,]) =
+        let imgH = img.GetLength 0
+        let imgW = img.GetLength 1
+
+        let filterD = Array.length filter / 2
+        let filter = Array.concat filter
+
+        let halfImgH = imgH / 2
+        let halfImgW = imgW / 2
+
+        let res = Array2D.create imgH imgW (Rgba32(0.0f, 0.0f, 0.0f, 0.0f))
+
+        Parallel.For (0, 4, fun k -> 
+            if k = 0 then 
+                for i in 0 .. halfImgH - 1 do 
+                    for j in 0 .. halfImgW - 1 do 
+                        let r, g, b = processPixel i j filter filterD img imgH imgW
+                        res.[i, j] <- Rgba32(r, g, b, img.[i, j].A)
+
+            elif k = 1 then 
+                for i in halfImgH .. imgH - 1 do 
+                    for j in 0 .. halfImgW - 1 do 
+                        let r, g, b = processPixel i j filter filterD img imgH imgW
+                        res.[i, j] <- Rgba32(r, g, b, img.[i, j].A)                
+            
+            elif k = 2 then 
+                for i in 0 .. halfImgH - 1 do 
+                    for j in halfImgW .. imgW - 1 do 
+                        let r, g, b = processPixel i j filter filterD img imgH imgW
+                        res.[i, j] <- Rgba32(r, g, b, img.[i, j].A)   
+
+            else 
+                for i in halfImgH .. imgH - 1 do 
+                    for j in halfImgW .. imgW - 1 do 
+                        let r, g, b = processPixel i j filter filterD img imgH imgW
+                        res.[i, j] <- Rgba32(r, g, b, img.[i, j].A)  
+        ) |> ignore
+        res
+
+    let applyFilterRowParallelism (filter: float32[][]) (img: Rgba32[,]) =
+        let imgH = img.GetLength 0
+        let imgW = img.GetLength 1
+
+        let filterD = Array.length filter / 2
+        let filter = Array.concat filter
+
+        let res = Array2D.create imgH imgW (Rgba32(0.0f, 0.0f, 0.0f, 0.0f))
+
+        Parallel.For (0, imgH, fun k -> 
+            for i in 0 .. imgW - 1 do 
+                let r, g, b = processPixel k i filter filterD img imgH imgW
+                res.[k, i] <- Rgba32(r, g, b, img.[k, i].A)      
+        ) |> ignore
+        res
+
+    let applyFilterColParallelism (filter: float32[][]) (img: Rgba32[,]) =
+        let imgH = img.GetLength 0
+        let imgW = img.GetLength 1
+
+        let filterD = Array.length filter / 2
+        let filter = Array.concat filter
+
+        let res = Array2D.create imgH imgW (Rgba32(0.0f, 0.0f, 0.0f, 0.0f))
+
+        Parallel.For (0, imgW, fun k -> 
+            for i in 0 .. imgH - 1 do 
+                let r, g, b = processPixel i k filter filterD img imgH imgW
+                res.[i, k] <- Rgba32(r, g, b, img.[i, k].A)      
+        ) |> ignore
+        res
